fa918dfd71dcd2c24b9bcd49904d65b5b445e904
ayaka126 <itchy.ayaka@gmail.com>
2023-08-03 12:15:02 +0900

    N+1問題解決のためcomments_controllerでincludeメソッド追加、分かりやすくするため@post_commentsを@commentsへ修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
-    @post_comments = @post.comments 
-
+    @comments = @post.comments.includes(:user).order(created_at: :desc)
   end
 
   def new
