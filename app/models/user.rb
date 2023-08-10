73c4dde98532f56b28a72fb34c88c9b6a030e512
ayaka126 <itchy.ayaka@gmail.com>
2023-08-10 13:07:19 +0900

    Userモデルのmodel spec実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ApplicationRecord
   has_one_attached :userimage
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_secure_password
 
   validates :username, presence: true, length: { minimum: 1, maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  has_secure_password
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む6文字以上で設定してください" }
-  
+  validates :password, confirmation: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む6文字以上で設定してください" }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    username { "testuser" }
+    email { "test@test.com" } 
+    password { "testpassword123" }
+    password_confirmation { "testpassword123" }
   end
 end
