3575e369c5f2e85c6e0ed6dc7cc41b2590fb81a3
ayaka126 <itchy.ayaka@gmail.com>
2023-08-23 21:40:55 +0900

    postシステムスペックに投稿詳細ページのテストを追加

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -6,54 +6,131 @@ RSpec.describe "Posts", type: :system do
   let!(:post){ create(:post, user: user, facility: facility) }
   let!(:comment){ create(:comment, user: user, post: post)}
 
-  before do
-    visit login_path
-    fill_in "email", with: user.email
-    fill_in "password", with: user.password
-    within ".login_button" do
-      click_on "ログイン"
+  describe "投稿一覧ページ" do
+    before do
+      visit login_path
+      fill_in "email", with: user.email
+      fill_in "password", with: user.password
+      within ".login_button" do
+        click_on "ログイン"
+      end
+      visit facility_posts_path(post.facility)
     end
-    visit facility_posts_path(post.facility)
+
+    it '保育園名が表示されていること' do
+      expect(page).to have_content facility.name
+    end
+
+    it '投稿一覧の各項目が表示されていること' do
+      expect(page).to have_content "タイトル"
+      expect(page).to have_content "内容"
+      expect(page).to have_content "詳細"
+      expect(page).to have_content "投稿日"
+    end
+
+    it '投稿一覧の各項目の内容が表示されていること' do
+      expect(page).to have_content post.title
+      expect(page).to have_content post.content
+      expect(page).to have_content "コメントを見る（1）"
+      expect(page).to have_content post.created_at.strftime('%Y/%m/%d %H:%M')
+    end
+
+    it '施設一覧へ戻るリンクが表示されていること' do
+      expect(page).to have_content "施設一覧へ戻る"
+    end
+
+    it '施設一覧へ戻るを押下すると施設一覧ページへ遷移すること' do
+      click_on "施設一覧へ戻る"
+      expect(current_path).to eq facilities_path
+    end
+
+    it '新規投稿するボタンが表示されていること' do
+      expect(page).to have_content "新規投稿する"
+    end
+
+    it '新規投稿するボタンを押下すると新規投稿ページへ遷移すること' do
+      click_on "新規投稿する"
+      expect(current_path).to eq new_facility_post_path(facility)
+    end
+
+    it 'コメントを見るを押下するとコメントを含む投稿の詳細画面へ遷移すること' do
+      click_on "コメントを見る（1）"
+      expect(current_path).to eq facility_post_path(post.facility, post)
+    end
   end
 
-  it '保育園名が表示されていること' do
-    expect(page).to have_content facility.name
-  end
+  describe "投稿詳細ページ" do
+    before do
+      visit login_path
+      fill_in "email", with: user.email
+      fill_in "password", with: user.password
+      within ".login_button" do
+        click_on "ログイン"
+      end
+      visit facility_post_path(post.facility, post)
+    end
 
-  it '投稿一覧の各項目が表示されていること' do
-    expect(page).to have_content "タイトル"
-    expect(page).to have_content "内容"
-    expect(page).to have_content "詳細"
-    expect(page).to have_content "投稿日"
-  end
+    it '保育園名が表示されていること' do
+      expect(page).to have_content facility.name
+    end
 
-  it '投稿一覧の各項目の内容が表示されていること' do
-    expect(page).to have_content post.title
-    expect(page).to have_content post.content
-    expect(page).to have_content "コメントを見る（1）"
-    expect(page).to have_content post.created_at.strftime('%Y/%m/%d %H:%M')
-  end
+    it '投稿者のusernameと投稿日が表示されていること' do
+      expect(page).to have_content post.user.username
+      expect(page).to have_content post.created_at.strftime('%Y/%m/%d %H:%M')
+    end
 
-  it '施設一覧へ戻るリンクが表示されていること' do
-    expect(page).to have_content "施設一覧へ戻る"
-  end
+    it '投稿の削除ボタンが表示されていること' do
+      within ".post-field" do
+        expect(page).to have_button "削除"
+      end
+    end
 
-  it '施設一覧へ戻るを押下すると施設一覧ページへ遷移すること' do
-    click_on "施設一覧へ戻る"
-    expect(current_path).to eq facilities_path
-  end
+    it '投稿の削除ボタンを押下すると投稿が削除されること' do
+      within ".post-field" do
+        click_on "削除"
+      end
+      expect(page).to have_content "投稿が削除されました"
+      expect(current_path).to eq facility_posts_path(facility)
+    end
 
-  it '新規投稿するボタンが表示されていること' do
-    expect(page).to have_content "新規投稿する"
-  end
+    it 'コメントフィールドが表示されていること' do
+      expect(page).to have_field("comment_content")
+    end
 
-  it '新規投稿するボタンを押下すると新規投稿ページへ遷移すること' do
-    click_on "新規投稿する"
-    expect(current_path).to eq new_facility_post_path(facility)
-  end
+    it 'コメントするボタンが表示されていること' do
+      expect(page).to have_button "コメントする"
+    end
 
-  it 'コメントを見るを押下するとコメントを含む投稿の詳細画面へ遷移すること' do
-    click_on "コメントを見る（1）"
-    expect(current_path).to eq facility_post_path(post.facility, post)
+    it 'コメントをしたusernameと投稿日が表示されていること' do
+      expect(page).to have_content comment.user.username
+      expect(page).to have_content comment.created_at.strftime('%Y/%m/%d %H:%M')
+    end
+
+    it 'コメント内容が表示されていること' do
+      expect(page).to have_content comment.content
+    end
+
+    it 'コメントの削除ボタンが表示されること' do
+      within ".comment-field" do
+        expect(page).to have_button "削除"
+      end
+    end
+
+    it 'コメントの削除ボタンを押下するとコメントが削除されること' do
+      within ".comment-field" do
+        click_on "削除"
+      end
+      expect(page).to have_content "コメントを削除しました"
+      expect(page).to have_content "コメントはまだありません"
+    end
+
+    it '投稿一覧へ戻るボタンが表示されていること' do
+      expect(page).to have_content "投稿一覧へ戻る"
+    end
+
+    it '投稿一覧へ戻るボタンを押下すると投稿一覧ページへ遷移すること' do
+      click_on "投稿一覧へ戻る"
+      expect(current_path).to eq facility_posts_path(facility)
+    end
   end
 end
