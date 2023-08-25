59d0d582b5e0250b2e3bff5d1991fc4146020aed
ayaka126 <itchy.ayaka@gmail.com>
2023-08-25 19:14:11 +0900

    ユーザー情報編集時にパスワード不要になるようバリデーションにon createを追加、ビューからパスワード項目を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 1, maximum: 30 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i.freeze
-  validates :password, confirmation: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む6文字以上で設定してください" }
+  validates :password, confirmation: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む6文字以上で設定してください" }, on: :create
 end
