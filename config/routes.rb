bf7aac995a7da6357021ae6866e56e95108196b4
ayaka126 <itchy.ayaka@gmail.com>
2023-08-26 23:43:29 +0900

    ゲストログイン機能追加/トップページ、ヘッダーにゲストログインボタン追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get    '/indexbymap', to:'facilities#indexbymap'
+  post   'guest_login', to: "guest_sessions#create"
   resource :user
   resources :facilities do
     collection do
