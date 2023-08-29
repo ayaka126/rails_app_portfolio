cc7c17723872391c72a21f9bea4aad26e122b992
ayaka126 <itchy.ayaka@gmail.com>
2023-08-30 08:20:14 +0900

    viewのbootstrapとcssの調整、統一

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_facility
   before_action :set_user
+  before_action :set_post, only: [:edit, :update]
 
   def index
     @posts = Post.all
@@ -27,6 +28,17 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to facility_post_path(@facility, @post), notice: "投稿内容を変更しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @post = Post.find(params[:id])
     if @post.destroy
@@ -47,5 +59,9 @@ class PostsController < ApplicationController
 
   def set_user
     @user = User.find(session[:user_id])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
