48e96072953af1b8d93371c73077007e7a197d6c
ayaka126 <itchy.ayaka@gmail.com>
2023-08-30 11:36:28 +0900

    ゲストログイン機能のシステムスペック追加、rubocopの指摘によりguest_sessions_controllerの変数を修正

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,10 +1,10 @@
 class GuestSessionsController < ApplicationController
   def create
-    user = User.find_or_create_by(email: "guest@example.com") do |user|
+    @user = User.find_or_create_by(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
       user.username = "ゲストユーザー"
     end
-    session[:user_id] = user.id
+    session[:user_id] = @user.id
     flash[:notice] = "ゲストユーザーとしてログインしました"
     redirect_to root_path
   end
